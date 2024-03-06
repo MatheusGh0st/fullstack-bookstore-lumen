628a852019bcd151fcaf50f81721324056458ba4
MatheusGh0st <matheusghost88@gmail.com>
2024-03-06 04:14:59 -0300

    add new images and refactor components

--- resources/js/vue/components/Navbar.vue
+++ resources/js/vue/components/Navbar.vue
@@ -8,7 +8,11 @@ export default {
     },
     data() {
         return {
-            imgCartUrl: 'https://thecleverheart.com/wp-content/uploads/2023/10/SHOPPING-CART-light-grey-app-icon.png',
+            imgFavBookUrl: '/images/bx_bx-book-heart.svg',
+            imgNotificationUrl: 'images/ic_round-notifications-none.svg',
+            imgAvatarUrl: '/images/avatar.svg',
+            iconBookStoreUrl: '/images/icon-bookstore.jpeg',
+            imgCartUrl: '/images/cart-default.jpeg',
         }
     },
 }
@@ -16,41 +20,46 @@ export default {
 
 <template>
     <nav>
-        <div>
-            <img />
-        </div>
         <ul>
-            <li><router-link to="/home">Home</router-link></li>
-            <li><router-link to="/cart"><img :src=imgCartUrl width="48" height="48" /></router-link></li>
+            <li><router-link to="/home"><img :src=iconBookStoreUrl /></router-link></li>
         </ul>
         <ul v-show="!$store.state.isLogged">
             <li><router-link to="/login">Login</router-link></li>
             <li><router-link to="/register">Register</router-link></li>
+        </ul>
+        <ul v-show="$store.state.isLogged">
+            <li><router-link to="/notifications"><img :src=imgNotificationUrl /></router-link></li>
+            <li><router-link to="/favorites"><img :src=imgFavBookUrl /></router-link></li>
+            <li><router-link to="/cart"><img :src=imgCartUrl  /></router-link></li>
+            <li><router-link to="me"><img :src=imgAvatarUrl /></router-link></li>
         </ul>
     </nav>
 </template>
 
 <style scoped>
 nav {
-    height: 70px;
+    height: 75px;
     display: flex;
-    padding: 20px 36px;
+    padding: 0px 20px 0px 20px;
+    background-color: #292828;
     box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.1);
+}
+
+img {
+    height: 48px;
+    width: 48px;
 }
 
 li {
     list-style: none;
     width: 100%;
     border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
-    padding: 0px 25px 20px 20px;
+    padding: 0px 20px 0px 20px;
 }
 
 ul {
     display: flex;
     align-content: space-between;
-}
-
-.nav--logo {
-    max-width: 100px;
+    margin: 0px;
 }
 </style>
