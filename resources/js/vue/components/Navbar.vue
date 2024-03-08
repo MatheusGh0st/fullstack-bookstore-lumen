5cf92dc6581a150ab663faf3cd21b3a9088f88d1
MatheusGh0st <matheusghost88@gmail.com>
2024-03-08 08:49:56 -0300

    change image add css styles and refactor components

--- resources/js/vue/components/Navbar.vue
+++ resources/js/vue/components/Navbar.vue
@@ -11,7 +11,7 @@ export default {
             imgFavBookUrl: '/images/bx_bx-book-heart.svg',
             imgNotificationUrl: 'images/ic_round-notifications-none.svg',
             imgAvatarUrl: '/images/avatar.svg',
-            iconBookStoreUrl: '/images/icon-bookstore.jpeg',
+            iconBookStoreUrl: '/images/online-bookstore.jpeg',
             imgCartUrl: '/images/cart-default.jpeg',
         }
     },
@@ -19,13 +19,17 @@ export default {
 </script>
 
 <template>
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
     <nav>
         <ul>
-            <li><router-link to="/home"><img :src=iconBookStoreUrl /></router-link></li>
+            <li><router-link to="/home"><img class="img-bookstore" :src=iconBookStoreUrl />BookStore</router-link></li>
         </ul>
-        <ul v-show="!$store.state.isLogged">
-            <li><router-link to="/login">Login</router-link></li>
-            <li><router-link to="/register">Register</router-link></li>
+        <ul>
+            <li><input type="text" placeholder="Search..."></li>
+        </ul>
+        <ul class="ul-register" v-show="!$store.state.isLogged">
+            <li class="li-btn-register"><router-link to="/login">Login</router-link></li>
+            <li class="li-btn-register"><router-link to="/register">Register</router-link></li>
         </ul>
         <ul v-show="$store.state.isLogged">
             <li><router-link to="/notifications"><img :src=imgNotificationUrl /></router-link></li>
@@ -37,17 +41,75 @@ export default {
 </template>
 
 <style scoped>
+* {
+    font-family: 'Ubuntu';
+}
+
 nav {
-    height: 75px;
     display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
+    height: 75px;
     padding: 0px 20px 0px 20px;
     background-color: #292828;
-    box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.1);
+}
+
+.li-btn-register {
+    background-color: #B6B6B6;
+    color: #EAEAEA;
+    padding: 12px 20px;
+    margin-left: 15px;
+    border-radius: 5px;
+}
+
+.li-btn-register:hover {
+    cursor: pointer;
+    background-color: #d3cdcd;
+}
+
+input {
+    width: 500px;
+    height: 30px;
+    background: #373737 url("/images/search-outline.svg") no-repeat 10px center;
+    margin: 10px;
+    padding: 10px 35px;
+    background-size: 20px 20px;
+    font-size: 16px;
+    border: none;
+    border-radius: 5px;
+    color: #808080;
+}
+
+.ul-register > li {
+    font-weight: bold;
+}
+
+input:focus {
+    outline: none;
 }
 
 img {
-    height: 48px;
-    width: 48px;
+    height: 36px;
+    width: 36px;
+}
+
+.img-bookstore {
+    height: 64px;
+    width: 64px;
+    background: #B6B6B6;
+}
+
+.img-bookstore:hover {
+    cursor: pointer;
+    background-color: #d3cdcd;
+}
+
+a, a:active, a:hover {
+    color: white;
+    text-decoration: none;
 }
 
 li {
