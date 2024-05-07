6e071cec5ad555c1f0e5531e7c2bf7769661a28a
MatheusGh0st <matheusghost88@gmail.com>
2024-05-07 14:28:49 -0300

    Add tests for CityController

--- tests/City/CityControllerTest.php
+++ tests/City/CityControllerTest.php
@@ -2,9 +2,64 @@
 
 namespace City;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\City;
+use PHPUnit\Exception;
+use TestCase;
 
 class CityControllerTest extends TestCase
 {
+    public function testCityReturnAll()
+    {
+        $token = getenv('AUTH_TOKEN');
 
+        try {
+            $response = $this->get('/cities', [
+                'auth_bearer' => $token,
+            ]);
+
+            $response->assertResponseStatus(200);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function testCitiesIsCreatedSuccessfully()
+    {
+        $token = getenv('AUTH_TOKEN');
+
+        $city = City::query()->create(City::factory()->make()->getAttributes());
+
+        $payload = [
+            'city_name' => $city->city_name,
+            'country_id' => random_int(0, 10)
+        ];
+
+        try {
+            $response = $this->post('/city', $payload, [
+                'auth_bearer' => $token,
+            ]);
+
+            $response->assertResponseStatus(200);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function testDestroyCity()
+    {
+        $token = getenv('AUTH_TOKEN');
+
+        $cityAttributes = City::factory()->make()->getAttributes();
+        $city = City::query()->create($cityAttributes);
+
+        try {
+            $response = $this->post("/city/$city->id", [], [
+                'auth_bearer' => $token
+            ]);
+
+            $response->assertResponseStatus(200);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
 }
