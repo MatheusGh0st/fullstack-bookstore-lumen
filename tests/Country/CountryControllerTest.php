af2f824ad25c46cd25eb56568adead70873bb455
MatheusGh0st <matheusghost88@gmail.com>
2024-05-10 11:14:14 -0300

    add tests for CountryController

--- tests/Country/CountryControllerTest.php
+++ tests/Country/CountryControllerTest.php
@@ -2,9 +2,63 @@
 
 namespace Country;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Country;
+use PHPUnit\Exception;
+use TestCase;
 
 class CountryControllerTest extends TestCase
 {
+    public function testReturnAllCountries()
+    {
+        $token = getenv('AUTH_TOKEN');
 
+        try {
+            $response = $this->get('/countries', [
+                'auth_bearer' => $token
+            ]);
+
+            $response->assertResponseStatus(200);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function testCountriesCreatedSuccessfully()
+    {
+        $token = getenv('AUTH_TOKEN');
+
+        $country = Country::query()->create(Country::factory()->make()->getAttributes());
+
+        $payload = [
+            'country_name' => $country->country_name
+        ];
+
+        try {
+            $response = $this->post('/country', $payload, [
+                'auth_bearer' => $token
+            ]);
+
+            $response->assertResponseStatus(201);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function testDestroyCountry()
+    {
+        $token = getenv('AUTH_TOKEN');
+
+        $countryAttributes = Country::factory()->make()->getAttributes();
+        $country = Country::query()->create($countryAttributes);
+
+        try {
+            $response = $this->post("/city/$country->id", [], [
+                'auth_bearer' => $token
+            ]);
+
+            $response->assertResponseStatus(200);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
 }
