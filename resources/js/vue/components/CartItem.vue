fcd647e5182c600ca69ff20d009f1e012ebc0685
MatheusGh0st <matheusghost88@gmail.com>
2024-06-04 23:10:23 -0300

    add new features to cartItem component

--- resources/js/vue/components/CartItem.vue
+++ resources/js/vue/components/CartItem.vue
@@ -5,9 +5,16 @@ import { useStore } from "vuex";
 
 export default {
     name: "Cart",
+    data() {
+        return {
+            imageUrl: 'https://www.boldstrokesbooks.com/assets/bsb/images/book-default-cover.jpg',
+        }
+    },
     setup() {
         const store = useStore();
         const carts = ref([]);
+        const books = ref({});
+        const total_price = ref(0.0);
 
         const getUserCarts = async () => {
             const userId = store.state.userId;
@@ -23,28 +30,93 @@ export default {
             }
         }
 
+        const getBookById = async (id) => {
+            try {
+                const response = await axios.get(`http://localhost:5000/book/${id}`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + store.state.accessToken
+                    }
+                });
+                return response.data.data;
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
         return {
             carts,
+            books,
+            total_price,
+            getBookById,
             getUserCarts,
         }
     },
-    mounted() {
-        this.getUserCarts();
+    computed: {
+        async fetchedCarts() {
+            await this.getUserCarts();
+            return this.carts;
+        }
+    },
+
+    async mounted() {
+        const cartsDetails = await this.fetchedCarts;
+        this.books = await Promise.all(cartsDetails.map(cartDetail => this.getBookById(cartDetail.book_id)));
+        this.calculateTotal();
+    },
+    methods: {
+        calculateTotal() {
+            this.total_price = 0;
+            this.books.forEach(book => {
+                this.total_price += book.price;
+            });
+        }
     }
 }
 </script>
 
 <template>
-    <li class="cart-box" v-for="cart in carts" :key="cart_id">
-        <div class="cart-item-container">
-            <div>Total Books: {{ cart.total_books }}</div>
-            <div>Discount: {{ cart.discount }}</div>
+    <div class="cart-item-container">
+        <div class="cart-container">
+            <li class="book-product-container" v-for="book in books">
+                <!--
+                <div class=book-img> <img :src=imageUrl width=60 height=80 /> </div>
+                -->
+
+                <div> {{ book.title }} </div>
+                <div> {{ book.stock }} </div>
+                <div> {{ book.status }} </div>
+                <div> {{ book.genre }} </div>
+                <div> {{ book.edition }} </div>
+                <div> {{ book.price }} </div>
+            </li>
+            <hr>
+            <div>Total: {{ total_price.toFixed(2) }}</div>
         </div>
-    </li>
+    </div>
 </template>
 
 <style scoped>
-.cart-box {
+.cart-item-container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    height: 300vh;
     background-color: #242121;
+}
+
+.cart-container {
+    border: 1px solid black;
+}
+
+.book-product-container {
+
+    border: 1px solid black;
+}
+
+li {
+    list-style: none;
 }
 </style>
